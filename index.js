a23b438895e4c8e1f30202c505411756ade25fb8
Zhen Liu <zhenmasta@gmail.com>
2020-11-22 02:35:22 -0500

    Mongoose Part 2

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
+    /* First option
     var newDish = Dishes({
         name: 'Uthappizza',
         description: 'test'
@@ -31,5 +32,25 @@ connect.then((db) => {
         .catch((err) => {
             console.log(err);
         });
+    */
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
 
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
